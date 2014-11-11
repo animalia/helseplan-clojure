327b5a9cce0cf78e40e0e7a59383c9f3a176e07e
rundis <mrundberget@hotmail.com>
2014-11-11 13:58:10 +0100

    Fikset datasource til å være inmemory

--- src/helseplan/datasource.clj
+++ src/helseplan/datasource.clj
@@ -1,7 +1,7 @@
 (ns helseplan.datasource
   (:require [hikari-cp.core :refer :all]))
 
-(def config {:url "jdbc:h2:~/test"
+(def config {:url "jdbc:h2:mem"
              :adapter :h2})
 
 (def ds-config (datasource-config config))
