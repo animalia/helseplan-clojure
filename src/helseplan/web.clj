0d7b2a92b44c250937c470cdec0b8b2b637f654b
rundis <mrundberget@hotmail.com>
2014-11-12 10:29:33 +0100

    Håndterer endre medlem som fake put og innført simpel validering

--- src/helseplan/web.clj
+++ src/helseplan/web.clj
@@ -1,12 +1,61 @@
 (ns helseplan.web
   (:require [compojure.core :refer [defroutes GET ANY]]
             [ring.middleware.params :refer [wrap-params]]
-            [ring.middleware.session :refer [wrap-session]]
             [ring.adapter.jetty :as ring]
-            [liberator.core :refer [resource defresource]]
+            [liberator.core :refer [resource defresource by-method]]
             [selmer.parser :refer [render-file]]
             [helseplan.datasource :as datasource]
-            [helseplan.db :as db]))
+            [helseplan.db :as db]
+            [schema.core :as s]
+            [schema.coerce :as coerce]))
+
+
+(defn wrap-fake-methods
+  "Middleware to wrap a handler to use the HTTP request method
+  specified in the __method query or post parameter if it exists.
+  Note that this middleware must be called after the query and/or post
+  parameters are parsed using ring.middleware/wrap-params or similar."
+  [hdlr]
+  (fn [req]
+
+    (if-let [request-method (and (= :post (:request-method req))
+                                 (or (get-in req [:params :__method])
+                                     (get-in req [:params "__method"])))]
+      (hdlr (assoc req :request-method (keyword (clojure.string/lower-case request-method))))
+      (hdlr req))))
+
+
+(defn simple-coercion [schema]
+  (s/start-walker
+   (fn [sc]
+     (let [walk (s/walker sc)]
+       (fn [x]
+         (if (and (= sc s/Keyword) (string? x))
+           (walk (keyword x))
+           (walk x)))))
+   schema))
+
+(defn length-greater [l]
+  (s/pred
+   (fn [x]
+     (> (count x) l)) (str "Lengden må være større enn " l)))
+
+
+
+
+(def MedlemSchema
+  "Skjema for validering av gyldig medlem"
+  {:id s/Num
+   :fornavn (s/both s/Str (length-greater 0))
+   :etternavn (s/both s/Str (length-greater 0))
+   :prodnr (s/both s/Str (length-greater 0))
+   s/Any s/Any})
+
+(defn parse-medlem-req [data]
+  (->>
+   data
+   ((simple-coercion {s/Keyword s/Any}))
+   ((coerce/coercer MedlemSchema coerce/string-coercion-matcher))))
 
 
 
@@ -26,23 +75,38 @@
   :handle-ok (fn [ctx]
                (let [media-type (get-in ctx [:representation :media-type])
                      medlem (db/hent-medlem (datasource/get-ds) id)]
-                 (println session)
                  (condp = media-type
                    "text/html" (render-file "templates/medlem.html" {:medlem medlem :header "Endre medlem" :method "PUT"})
                    medlem)))
-  :handle-errors (fn [x] (println x))
-  :post! (fn [ctx]
-           ;;En post som burde kunne vært en put men akk!
-           (let [params (get-in ctx [:request :params])]
-             (try
-               (db/oppdater-medlem! (datasource/get-ds) (dissoc params "_method"))
-               (catch Exception e
-                 (println e))) ; TODO: Må finne ut hvordan man kan få ut exception info fra liberator !
-             {::id id}))
-  :post-redirect? (fn [ctx] {:location (format "/medlemmer/%s" (::id ctx))})
+  :processable?  (by-method {:get true
+                             :put (fn [ctx]
+                                    (let [params (get-in ctx [:request :params])]
+                                      (not (:error (parse-medlem-req params)))))})
+
+
+  :handle-unprocessable-entity (fn [ctx]
+                                 (let [params ((simple-coercion {s/Keyword s/Any})(get-in ctx [:request :params]))
+                                       errors (parse-medlem-req params)]
+                                   (render-file "templates/medlem.html" {:medlem params
+                                                                         :header "Endre medlem"
+                                                                         :method "PUT"
+                                                                         :errors (:error errors)})))
   :put! (fn [ctx]
-          (println ctx)
-          "TODO : Oppdatert medlem"))
+          (let [params (get-in ctx [:request :params])]
+            (try
+              (db/oppdater-medlem! (datasource/get-ds) (dissoc params "__method"))
+              (catch Exception e
+                (println e))) ; TODO: Må finne ut hvordan man kan få ut exception info fra liberator !
+            ))
+  :handle-created (fn [ctx]
+                    (let [params ((simple-coercion {s/Keyword s/Any})(get-in ctx [:request :params]))]
+                      (render-file "templates/medlem.html" {:medlem params
+                                                            :header "Endre medlem"
+                                                            :method "PUT"})))
+  :post-redirect? (fn [ctx] {:location (format "/medlemmer/%s" (::id ctx))})
+  :post! (fn [ctx]
+           (println ctx)
+           "TODO : Oppdatert medlem"))
 
 
 (defroutes routes
@@ -55,5 +119,5 @@
 
 
 (def app (-> routes
-             wrap-params
-             wrap-session))
+             wrap-fake-methods
+             wrap-params))
